01a63f24892ef21993f241dd8d9cecaabf6a3609
Saeid Noroozi <s.noroozi.cs@gmail.com>
2025-07-25 15:59:21 +0330

    Update tips.sql

--- sessions/07/04/tips.sql
+++ sessions/07/04/tips.sql
@@ -40,6 +40,102 @@ cqlsh> REVOKE role_name FROM 'username';
 
 -----------------------------------------------------------------
 
+cqlsh> LIST ROLES;
+
+
+Key Differences Between Roles and Users
+-----------------------------------------------------------------------
+Feature	            Traditional Users	            Roles
+Authentication	    Username/password only	        Can represent both users and groups
+Authorization	    Direct permissions only	        Inheritable permissions
+Structure	        Flat hierarchy	                Hierarchical (role inheritance)
+Flexibility     	Limited	                        Can represent service accounts, groups, or individuals
+
+
+Roles as Users
+--------------------------------------------------
+cqlsh> CREATE ROLE web_app 
+        WITH PASSWORD = 'secure123' 
+        AND LOGIN = true;
+
+
+Roles as Groups
+--------------------------------------------------
+cqlsh> CREATE ROLE analytics_team 
+        WITH LOGIN = false;
+
+
+Inheritance
+--------------------------------------------------
+cqlsh> GRANT analytics_team TO data_scientist;
+
+
+
+Creating a Login Role (User)
+--------------------------------------------------
+cqlsh> CREATE ROLE dba 
+        WITH PASSWORD = 'dbapass' 
+        AND LOGIN = true 
+        AND SUPERUSER = true;
+
+
+
+Creating a Group Role
+--------------------------------------------------
+cqlsh> CREATE ROLE read_only_users;
+cqlsh> GRANT SELECT ON ALL KEYSPACES TO read_only_users;
+
+
+
+Assigning Group Membership
+--------------------------------------------------
+cqlsh> CREATE ROLE dba_L1 
+        WITH PASSWORD = 'dba_L1' 
+        AND LOGIN = true;
+cqlsh> GRANT read_only_users TO dba_L1;
+
+
+
+
+Permission Management
+--------------------------------------------------
+-- Grant table access
+cqlsh> GRANT SELECT ON KEYSPACE retail TO inventory_reader;
+
+-- Revoke permissions
+cqlsh> REVOKE ALL PERMISSIONS ON KEYSPACE retail FROM inventory_reader;
+
+-- View permissions
+cqlsh> LIST ALL PERMISSIONS OF inventory_reader;
+
+
+
+Role Hierarchies
+--------------------------------------------------
+cqlsh> GRANT department_lead TO team_lead;
+cqlsh> GRANT team_lead TO staff_member;
+
+
+
+Best Practices
+--------------------------------------------------
+1. Use roles for applications: 
+    
+    cqlsh> CREATE ROLE inventory_service 
+            WITH PASSWORD = 'svcpass'
+
+2. Create functional groups: 
+    finance_team, 
+    dev_ops
+
+3. Limit SUPERUSER roles
+
+4. Regularly audit permissions with 
+    cqlsh> LIST ROLES
+    cqlsh> LIST PERMISSIONS
+
+
+
 
 
 
